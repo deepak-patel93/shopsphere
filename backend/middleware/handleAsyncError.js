5e855751431990f1d1b9937ab9435ce5ce197cba
Deepak patel <deepakkumarpatel42707@gmail.com>
2026-04-26 13:00:41 +0530

    modified:   backend/middleware/handleAsyncError.js

--- backend/middleware/handleAsyncError.js
+++ backend/middleware/handleAsyncError.js
@@ -1,7 +1,17 @@
-const handleAsyncError = (myErrorFun) => (req, res, next) => {
-  Promise.resolve(myErrorFun(req, res, next)).catch((err) => {
-    return next(err);
-  });
+const handleAsyncError = (fn) => {
+  return function (req, res, next) {
+    try {
+      return Promise.resolve(fn(req, res, next)).catch((err) => {
+        console.log("ERROR CAUGHT:", err);
+        console.log("TYPE OF next:", typeof next);
+        return next(err);
+      });
+    } catch (err) {
+      console.log("SYNC ERROR:", err);
+      console.log("TYPE OF next:", typeof next);
+      return next(err);
+    }
+  };
 };
 
 export default handleAsyncError;
