3d488adcb664c4fe83f770e12b9dcf74d8b84824
Deepak patel <deepakkumarpatel42707@gmail.com>
2026-04-26 10:56:49 +0530

    updation in handleAsyncError

--- backend/middleware/handleAsyncError.js
+++ backend/middleware/handleAsyncError.js
@@ -1,5 +1,7 @@
 const handleAsyncError = (myErrorFun) => (req, res, next) => {
-  Promise.resolve(myErrorFun(req, res, next)).catch((err) => next(err));
+  Promise.resolve(myErrorFun(req, res, next)).catch((err) => {
+    return next(err);
+  });
 };
 
 export default handleAsyncError;
